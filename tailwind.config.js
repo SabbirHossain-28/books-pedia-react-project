09df253fb8771eb10dd0f9c3f8bbadbfce21ec1b
Sabbir Hossain <sabbirhossainrabby1@gmail.com>
2024-03-26 11:03:43 +0600

    Create header component with navbar section and make responsive

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,12 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily:{
+        "work":["Work Sans, sans-serif"],
+        "playfair":["Playfair Display, serif"],
+      },
+    },
   },
   plugins: [require("daisyui")],
 }
