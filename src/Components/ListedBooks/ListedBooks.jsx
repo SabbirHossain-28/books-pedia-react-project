7cc45b02cd8a65e3d6bc29c90f8914fd484c832a
Sabbir Hossain <sabbirhossainrabby1@gmail.com>
2024-03-26 17:11:16 +0600

    Added tab component in ListedBooks section and set title for this section

--- src/Components/ListedBooks/ListedBooks.jsx
+++ src/Components/ListedBooks/ListedBooks.jsx
@@ -1,9 +1,57 @@
 const ListedBooks = () => {
-    return (
-        <div>
-            <h2>Hello this is listed books page</h2>
+  return (
+    <div className="max-w-7xl mx-auto">
+      <div className="border mt-9 mb-12 p-8 text-center bg-[#1313130D] rounded-lg">
+        <h2 className="text-[#131313] text-4xl font-work font-bold">Books</h2>
+      </div>
+      <div>
+        <div role="tablist" className="tabs tabs-lifted">
+          <input
+            type="radio"
+            name="my_tabs_2"
+            role="tab"
+            className="tab"
+            aria-label="Tab 1"
+          />
+          <div
+            role="tabpanel"
+            className="tab-content bg-base-100 border-base-300 rounded-box p-6"
+          >
+            <h2>hello world</h2>
+          </div>
+
+          <input
+            type="radio"
+            name="my_tabs_2"
+            role="tab"
+            className="tab"
+            aria-label="Tab 2"
+            checked
+          />
+          <div
+            role="tabpanel"
+            className="tab-content bg-base-100 border-base-300 rounded-box p-6"
+          >
+            Tab content 2
+          </div>
+
+          <input
+            type="radio"
+            name="my_tabs_2"
+            role="tab"
+            className="tab"
+            aria-label="Tab 3"
+          />
+          <div
+            role="tabpanel"
+            className="tab-content bg-base-100 border-base-300 rounded-box p-6"
+          >
+            Tab content 3
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default ListedBooks;
